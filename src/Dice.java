93193709083e05400271afb3b8d7b153f731124b
alialvi00 <ali_mashhood98@hotmail.com>
2020-10-23 23:53:15 -0400

    Dice class was updated with a getMethod which returns the generated Die value ranging from 1-6

--- src/Dice.java
+++ src/Dice.java
@@ -1,1 +1,32 @@
+import java.util.Random;
+
+/**
+ * This class is part of the "RISK: GLOBAL DOMINATION" application
+ *
+ * This class represents one die rolled. A random integer is generated
+ * using the Random class. Different Dice class objects can be used to represent
+ * dices in the game
+ *
+ * @author Ali, Areeb, Hassan, Raj
+ * @version Date: 2020-10-24
+ */
+
+public class Dice{
+
+    //the value that one die rolls
+    private int diceValue;
+    private static final int MINVALUE = 1;
+    private static final int MAXVALUE = 6;
+
+    public Dice(){
+
+        Random num = new Random(); //instance of Random class
+        diceValue = num.nextInt(MAXVALUE - MINVALUE + 1) + MINVALUE; //generate random integers from 1-6
+    }
+
+    public int getDiceValue(){
+        return diceValue;
+    }
+}
+
 
