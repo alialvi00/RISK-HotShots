236faef2d59968f3ef75915d609e108800cf6906
raj2000 <raj2000@rogers.com>
2020-10-24 01:12:49 -0400

    Implemented Country class and Continent Class. To provide information about the map in RISK.

--- src/Continent.java
+++ src/Continent.java
@@ -1,1 +1,27 @@
+/**
+ * Continent Class of the Continents in RISK. This class provides information about the continent's name,
+ * amount of reinforcement troops it is eligible for and how many countries it contains.
+ * @author Ali Alvi Areeb Haq Hassan Jallad Raj Sandhu
+ * @version 1.0
+ */
 
+
+public class Continent{
+
+    private String continentName;
+    private int numCountries;
+    private int extraTroops;
+
+    public Continent(String continentName, int numCountries, int extraTroops){
+
+        this.continentName = continentName;
+        this.numCountries = numCountries;
+        this.extraTroops = extraTroops;
+    }
+
+    public int getNumCountries(){
+
+        return numCountries;
+    }
+
+}
